1ac941abbb10a74acaeb354f459429df0b7ad349
Fernando Ramirez <f.e.ramirez94@gmail.com>
2016-09-05 11:09:10 -0300

    test

---
+++ app/src/main/java/inaka/com/testapp/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package inaka.com.testapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import inaka.com.testapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
